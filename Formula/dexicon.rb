18c2984a47cf3a2f72f4958f7506bdbb37c1bdf4
aaron-dexicon <aaron@dexicon.ai>
2025-12-01 17:06:42 -0800

    Added checksum for v0.2.1

--- Formula/dexicon.rb
+++ Formula/dexicon.rb
@@ -17,19 +17,19 @@ class Dexicon < Formula
   on_macos do
     on_arm do
       url "https://github.com/Dexicon-AI/get-dexicon-cli/releases/download/v#{version}/dexicon-cli-darwin-arm64"
-      sha256 "REPLACE_WITH_SHA256_DARWIN_ARM64"
+      sha256 "77bd3aacc2b2e4346ee3b6674b0e6a4b55f2009f2ca98a93fcfff7dc12425639"
     end
 
     on_intel do
       url "https://github.com/Dexicon-AI/get-dexicon-cli/releases/download/v#{version}/dexicon-cli-darwin-amd64"
-      sha256 "REPLACE_WITH_SHA256_DARWIN_AMD64"
+      sha256 "8b4c01e0bae20f79baf71a33e92b9953b713a31730c772d1b3b46a75331786a5"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/Dexicon-AI/get-dexicon-cli/releases/download/v#{version}/dexicon-cli-linux-amd64"
-      sha256 "REPLACE_WITH_SHA256_LINUX_AMD64"
+      sha256 "f833e994bd1c25b5e22b9e9d5a512d74ff38053d20935fad50c65c9c65e557c5"
     end
   end
 
